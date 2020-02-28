57fe86a92bd3f84d490675afa93eeb10e78c4f33
Logan Van Hook <loganvan@att.net>
2020-02-28 11:36:25 -0500

    Stage 1

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
+import { useHistory } from "react-router-dom";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+const Login = () =>
+{
+  let his = useHistory();
+
+  let [FormData, sFormData] = useState(
+    {
+      username:"",
+      password:"",
+      error:""
+    });
+
+  let SubmitForm = function()
+  {
+    Axios.post("http://localhost:5000/api/login", {username:FormData.username,password:FormData.password}).then((response)=>
+    {
+      window.localStorage.setItem("atk", response.data.payload);
+      his.push("/BubblePage");
+    }).catch((error)=>
+    {
+      sFormData({...FormData, error:error.response.data.error});
+    })
+  }
+
+  let updateFormData = function(e)
+  {
+    sFormData({...FormData, [e.target.name]:e.target.value});
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <div>
+        {FormData.error == "" ? null : FormData.error}
+        <form onSubmit={(e)=>{e.preventDefault();SubmitForm();}} onChange={(e)=>{updateFormData(e);}}>
+          <input type="text" name="username" placeholder="Username" defaultValue={FormData.username} />
+          <input type="password" name="password" placeholder="Password" defaultValue={FormData.password} />
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </>
   );
 };
